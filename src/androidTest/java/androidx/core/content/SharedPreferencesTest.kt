ccb132742744640b74eb338afb6263722b525c13
Paul-Marie Tetedoie <paul-marie.tetedoie@raizlabs.com>
2018-05-11 16:46:37 -0700

    add more assertion for operators

--- src/androidTest/java/androidx/core/content/SharedPreferencesTest.kt
+++ src/androidTest/java/androidx/core/content/SharedPreferencesTest.kt
@@ -50,25 +50,47 @@ class SharedPreferencesTest {
         val preferences = context.getSharedPreferences("set_prefs", 0)
 
         assertEquals(true, preferences[Boolean::class.java, "test_key1", true])
+        preferences.edit { putBoolean("test_key1", false) }
+        assertEquals(false, preferences[Boolean::class.java, "test_key1", true])
+
         assertEquals(0.1f, preferences[Float::class.java, "test_key2", 0.1f])
+        preferences.edit { putFloat("test_key2", 0.2f) }
+        assertEquals(0.2f, preferences[Float::class.java, "test_key2", 0.1f])
+
         assertEquals(100, preferences[Int::class.java, "test_key3", 100])
+        preferences.edit { putInt("test_key3", 101) }
+        assertEquals(101, preferences[Int::class.java, "test_key3", 100])
+
         assertEquals(123456789, preferences[Long::class.java, "test_key4", 123456789])
+        preferences.edit { putLong("test_key4", 123456788) }
+        assertEquals(123456788, preferences[Long::class.java, "test_key4", 123456789])
+
         assertEquals("test_value", preferences[String::class.java, "test_key5", "test_value"])
+        preferences.edit { putString("test_key5", "test") }
+        assertEquals("test", preferences[String::class.java, "test_key5", "default"])
     }
 
     @Test fun setOperator() {
         val preferences = context.getSharedPreferences("set_prefs", 0)
 
+        assertEquals(false, preferences.getBoolean("test_key1", false))
         preferences[Boolean::class.java, "test_key1"] = true
-        preferences[Float::class.java, "test_key2"] = 0.1f
-        preferences[Int::class.java, "test_key3"] = 100
-        preferences[Long::class.java, "test_key4"] = 123456789
-        preferences[String::class.java, "test_key5"] = "test_value"
-
         assertEquals(true, preferences.getBoolean("test_key1", false))
+
+        assertEquals(0.2f, preferences.getFloat("test_key2", 0.2f))
+        preferences[Float::class.java, "test_key2"] = 0.1f
         assertEquals(0.1f, preferences.getFloat("test_key2", 0.2f))
+
+        assertEquals(101, preferences.getInt("test_key3", 101))
+        preferences[Int::class.java, "test_key3"] = 100
         assertEquals(100, preferences.getInt("test_key3", 101))
+
+        assertEquals(123456788, preferences.getLong("test_key4", 123456788))
+        preferences[Long::class.java, "test_key4"] = 123456789
         assertEquals(123456789, preferences.getLong("test_key4", 123456788))
+
+        assertEquals("default", preferences.getString("test_key5", "default"))
+        preferences[String::class.java, "test_key5"] = "test_value"
         assertEquals("test_value", preferences.getString("test_key5", "default"))
     }
 
@@ -76,25 +98,47 @@ class SharedPreferencesTest {
         val preferences = context.getSharedPreferences("set_prefs", 0)
 
         assertEquals(true, preferences["test_key1", true])
+        preferences.edit { putBoolean("test_key1", false) }
+        assertEquals(false, preferences["test_key1", true])
+
         assertEquals(0.1f, preferences["test_key2", 0.1f])
+        preferences.edit { putFloat("test_key2", 0.2f) }
+        assertEquals(0.2f, preferences["test_key2", 0.1f])
+
         assertEquals(100, preferences["test_key3", 100])
+        preferences.edit { putInt("test_key3", 101) }
+        assertEquals(101, preferences["test_key3", 100])
+
         assertEquals(123456789, preferences["test_key4", 123456789])
-        assertEquals("test_value", preferences["test_key5", "test_value"])
+        preferences.edit { putLong("test_key4", 123456788) }
+        assertEquals(123456788, preferences["test_key4", 123456789])
+
+        assertEquals("default", preferences["test_key5", "default"])
+        preferences.edit { putString("test_key5", "test") }
+        assertEquals("test", preferences["test_key5", "default"])
     }
 
     @Test fun setInlinedOperator() {
         val preferences = context.getSharedPreferences("set_prefs", 0)
 
+        assertEquals(false, preferences.getBoolean("test_key1", false))
         preferences["test_key1"] = true
-        preferences["test_key2"] = 0.1f
-        preferences["test_key3"] = 100
-        preferences["test_key4"] = 123456789
-        preferences["test_key5"] = "test_value"
-
         assertEquals(true, preferences.getBoolean("test_key1", false))
+
+        assertEquals(0.2f, preferences.getFloat("test_key2", 0.2f))
+        preferences["test_key2"] = 0.1f
         assertEquals(0.1f, preferences.getFloat("test_key2", 0.2f))
+
+        assertEquals(101, preferences.getInt("test_key3", 101))
+        preferences["test_key3"] = 100
         assertEquals(100, preferences.getInt("test_key3", 101))
+
+        assertEquals(123456788, preferences.getLong("test_key4", 123456788))
+        preferences["test_key4"] = 123456789
         assertEquals(123456789, preferences.getLong("test_key4", 123456788))
+
+        assertEquals("default", preferences.getString("test_key5", "default"))
+        preferences["test_key5"] = "test_value"
         assertEquals("test_value", preferences.getString("test_key5", "default"))
     }
 }
